ad0920c8873b0f70ccd938b18aba80a7664d0535
Pablo Ariel Mayobre <pablo.a.mayobre@gmail.com>
2018-02-18 14:58:26 -0300

    Name all the quads!

--- src/quads.lua
+++ src/quads.lua
@@ -1,33 +1,65 @@
 local Quads = {}
 
-local sw, sh = 512, 512
+local quad, sw, sh = love.graphics.newQuad, 512, 512
 
-Quads["breakable_top_left"]     = love.graphics.newQuad(0,   0, 16, 16, 512, 512)
-Quads["breakable_top"]          = love.graphics.newQuad(16,  0, 16, 16, 512, 512)
-Quads["breakable_top_right"]    = love.graphics.newQuad(32,  0, 16, 16, 512, 512)
-Quads["breakable_left"]         = love.graphics.newQuad( 0, 16, 16, 16, 512, 512)
-Quads["breakable_middle"]       = love.graphics.newQuad(16, 16, 16, 16, 512, 512)
-Quads["breakable_right"]        = love.graphics.newQuad(32, 16, 16, 16, 512, 512)
-Quads["breakable_bottom_left"]  = love.graphics.newQuad( 0, 32, 16, 16, 512, 512)
-Quads["breakable_bottom"]       = love.graphics.newQuad(16, 32, 16, 16, 512, 512)
-Quads["breakable_bottom_right"] = love.graphics.newQuad(32, 32, 16, 16, 512, 512)
+local corners = {
+  '_top_left',    '_top',    '_top_right',
+  '_left',        '_middle', '_right',
+  '_bottom_left', '_bottom', '_bottom_right'
+}
+local function patch (name, x, y)
+  for i, corner in ipairs(corners) do
+    local offx, offy = (i - 1) % 3, math.floor(i/3)
+    Quads[name..corner] = quad(x + offx * 16, y + offy * 16, 16, 16, sw, sh)
+end
 
-Quads["breakable_top_left"]     = love.graphics.newQuad( 0,  0, 16, 16, 512, 512)
-Quads["breakable_top"]          = love.graphics.newQuad(16,  0, 16, 16, 512, 512)
-Quads["breakable_top_right"]    = love.graphics.newQuad(32,  0, 16, 16, 512, 512)
-Quads["breakable_left"]         = love.graphics.newQuad( 0, 16, 16, 16, 512, 512)
-Quads["breakable_middle"]       = love.graphics.newQuad(16, 16, 16, 16, 512, 512)
-Quads["breakable_right"]        = love.graphics.newQuad(32, 16, 16, 16, 512, 512)
-Quads["breakable_bottom_left"]  = love.graphics.newQuad( 0, 32, 16, 16, 512, 512)
-Quads["breakable_bottom"]       = love.graphics.newQuad(16, 32, 16, 16, 512, 512)
-Quads["breakable_bottom_right"] = love.graphics.newQuad(32, 32, 16, 16, 512, 512)
+patch('breakable',    0,  0)
+patch('platform',    48,  0)
+patch('metal',        0, 48)
+patch('background',  48, 48)
+patch('fade_border', 96,  0)
+patch('fade',        96, 48)
 
-Quads["cube"]  = love.graphics.newQuad(0, 96, 48, 48, 512, 512)
+Quads["cube"]  = quad(0, 96, 48, 48, sw, sh)
 
-Quads["spike_up"] = love.graphics.newQuad(144, 70, 16, 9, 512, 512)
+Quads["spike_up"]    = quad(144, 70, 16,  9, sw, sh)
+Quads["spike_right"] = quad(160, 64,  9, 16, sw, sh)
+Quads["spike_down"]  = quad(160, 80, 16,  9, sw, sh)
+Quads["spike_left"]  = quad(151, 80,  9, 16, sw, sh)
 
-Quads["lantern"]     = love.graphics.newQuad(130, 144, 16, 21, 512, 512)
-Quads["lantern_lit"] = love.graphics.newQuad(146, 144, 16, 21, 512, 512)
+Quads["lantern"]     = quad(130, 144, 16, 21, sw, sh)
+Quads["lantern_lit"] = quad(146, 144, 16, 21, sw, sh)
 
+Quads["light_1"] = quad(144,  0, 32, 32, sw, sh)
+Quads["light_2"] = quad(144, 32, 32, 32, sw, sh)
+
+Quads["background"] = quad(48, 96, 16, 16, sw, sh)
+
+Quads["arrow_left_lit"]  = quad( 0, 144, 16, 16, sw, sh)
+Quads["arrow_down_lit"]  = quad( 0, 160, 16, 16, sw, sh)
+Quads["arrow_right_lit"] = quad(16, 160, 16, 16, sw, sh)
+Quads["arrow_up_lit"]    = quad(16, 144, 16, 16, sw, sh)
+
+Quads["arrow_left"]  = quad(32, 144, 16, 16, sw, sh)
+Quads["arrow_down"]  = quad(32, 160, 16, 16, sw, sh)
+Quads["arrow_right"] = quad(48, 160, 16, 16, sw, sh)
+Quads["arrow_up"]    = quad(48, 144, 16, 16, sw, sh)
+
+Quads['background_flower']        = quad(64, 96, 32, 32, sw, sh)
+Quads['background_pillar_top']    = quad(64, 128, 32, 16, sw, sh)
+Quads['background_pillar_middle'] = quad(64, 144, 32, 16, sw, sh)
+Quads['background_pillar_bottom'] = quad(64, 160, 32, 16, sw, sh)
+
+Quads['flower']        = quad(96,  96, 32, 32, sw, sh)
+Quads['pillar_top']    = quad(96, 128, 32, 16, sw, sh)
+Quads['pillar_middle'] = quad(96, 144, 32, 16, sw, sh)
+Quads['pillar_bottom'] = quad(96, 160, 32, 16, sw, sh)
+
+Quads['glass_top']    = quad(144,  96, 16, 16, sw, sh)
+Quads['glass_middle'] = quad(144, 112, 16, 16, sw, sh)
+Quads['glass_bottom'] = quad(144, 128, 16, 16, sw, sh)
+
+Quads['glass_top_broken']    = quad(128,  96, 16, 16, sw, sh)
+Quads['glass_bottom_broken'] = quad(128, 128, 16, 16, sw, sh)
 
 return Quads
