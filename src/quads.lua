f5f216f5ab3604b0acd62b904e863404c0fe0984
Justin van der Leij <justinvanderleij@gmail.com>
2018-02-18 07:47:57 +0100

    Fixed quads AGAIN >:(

--- src/quads.lua
+++ src/quads.lua
@@ -2,12 +2,12 @@ local Quads = {}
 
 local sw, sh = 512, 512
 
-Quads["pattern"] = love.graphics.newQuad(0, 0, 32, 32, sw, sh)
+Quads["pattern"] = love.graphics.newQuad(96, 0, 32, 32, sw, sh)
 
-Quads["pillar_top"] = love.graphics.newQuad(0, 32, 32, 16, sw, sh)
-Quads["pillar_mid"] = love.graphics.newQuad(0, 48, 32, 16, sw, sh)
-Quads["pillar_bot"] = love.graphics.newQuad(0, 64, 32, 16, sw, sh)
+Quads["pillar_top"] = love.graphics.newQuad(96, 32, 32, 16, sw, sh)
+Quads["pillar_mid"] = love.graphics.newQuad(112, 48, 32, 16, sw, sh)
+Quads["pillar_bot"] = love.graphics.newQuad(128, 64, 32, 16, sw, sh)
 
-Quads["cube"] = love.graphics.newQuad(0, 80, 48, 48, sw, sh)
+Quads["cube"] = love.graphics.newQuad(128, 96, 48, 48, sw, sh)
 
 return Quads
