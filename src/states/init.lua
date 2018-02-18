3cc9c66cfc95613fc394160be5f3a059664a54e4
Justin van der Leij <justinvanderleij@gmail.com>
2018-02-18 07:46:40 +0100

    removed all the oop

--- src/states/init.lua
+++
@@ -1,4 +0,0 @@
-return {
-   menu = require("src.states.menu"),
-   game = require("src.states.game"),
-}
--- src/world.lua
+++
@@ -1,7 +0,0 @@
-local Bump   = require("libs.bump")
-local Vector = require("libs.vector")
-
-local World   = Bump.newWorld(96)
-World.gravity = Vector(0, 1000)
-
-return World
